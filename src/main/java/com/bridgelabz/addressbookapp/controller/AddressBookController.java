a41ff0a403a7074830653eee057c91708b800812
akspriya200408@gmail.com <akspriya200408@gmail.com>
2026-03-25 16:28:27 +0530

    [Akshaya Priya]:Added:UC6:validation and exception handling for AddressBook REST APIs

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -4,6 +4,7 @@ import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 import com.bridgelabz.addressbookapp.model.AddressBookData;
 import com.bridgelabz.addressbookapp.service.IAddressBookService;
 import com.bridgelabz.addressbookapp.util.ResponseDTO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,38 +18,34 @@ public class AddressBookController {
 
     @GetMapping
     public ResponseEntity<ResponseDTO> getAllContacts() {
-        ResponseDTO responseDTO = new ResponseDTO(
-                "Get Call Successful",
-                addressBookService.getAddressBookData()
-        );
+        ResponseDTO responseDTO = new ResponseDTO("Retrieved all contacts successfully",
+                addressBookService.getAddressBookData());
         return ResponseEntity.ok(responseDTO);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponseDTO> getContactById(@PathVariable int id) {
-        ResponseDTO responseDTO = new ResponseDTO(
-                "Get Call for ID Successful",
-                addressBookService.getAddressBookDataById(id)
-        );
+        ResponseDTO responseDTO = new ResponseDTO("Retrieved contact successfully",
+                addressBookService.getAddressBookDataById(id));
         return ResponseEntity.ok(responseDTO);
     }
 
-    @PostMapping
-    public ResponseEntity<ResponseDTO> createContact(@RequestBody AddressBookDTO addressBookDTO) {
-        AddressBookData data = addressBookService.createAddressBookData(addressBookDTO);
-        ResponseDTO responseDTO = new ResponseDTO("Created contact successfully", data);
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createContact(@Valid @RequestBody AddressBookDTO addressBookDTO) {
+        ResponseDTO responseDTO = new ResponseDTO("Created contact successfully",
+                addressBookService.createAddressBookData(addressBookDTO));
         return ResponseEntity.ok(responseDTO);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<ResponseDTO> updateContact(@PathVariable int id,
-                                                     @RequestBody AddressBookDTO addressBookDTO) {
-        AddressBookData data = addressBookService.updateAddressBookData(id, addressBookDTO);
-        ResponseDTO responseDTO = new ResponseDTO("Updated contact successfully", data);
+                                                     @Valid @RequestBody AddressBookDTO addressBookDTO) {
+        ResponseDTO responseDTO = new ResponseDTO("Updated contact successfully",
+                addressBookService.updateAddressBookData(id, addressBookDTO));
         return ResponseEntity.ok(responseDTO);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<ResponseDTO> deleteContact(@PathVariable int id) {
         addressBookService.deleteAddressBookData(id);
         ResponseDTO responseDTO = new ResponseDTO("Deleted contact successfully", "Deleted id: " + id);
--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,16 +1,26 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class AddressBookDTO {
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(
+            regexp = "^[A-Z][a-zA-Z\\s]{2,}$",
+            message = "Name should start with a capital letter and contain at least 3 characters"
+    )
     private String name;
-    private String city;
+
+    private String address;
     private String phoneNumber;
 
     public AddressBookDTO() {
     }
 
-    public AddressBookDTO(String name, String city, String phoneNumber) {
+    public AddressBookDTO(String name, String address, String phoneNumber) {
         this.name = name;
-        this.city = city;
+        this.address = address;
         this.phoneNumber = phoneNumber;
     }
 
@@ -22,12 +32,12 @@ public class AddressBookDTO {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public String getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getPhoneNumber() {
